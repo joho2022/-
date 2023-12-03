c60fe9a8df45dab695d4a092574a96eee41b889b
joho2022 <josama1997@naver.com>
2023-12-04 02:32:58 +0900

    Feat: 2단계 구현, 제거된 카드에서 다시 X표시가 나타나는 이슈 해결
    이미 제거된 카드 좌표와 뒤집힌 카드 좌표와 같이 고르면,
    제거된 카드에서 다시 X표시가 나타나는 이슈 해결
    
    resolves: #167

--- 카드맞추기게임TEST/마스터즈_카드맞추기게임/main.swift
+++ 카드맞추기게임TEST/마스터즈_카드맞추기게임/main.swift
@@ -63,52 +63,84 @@ func openCardBoard(cardBoard: [[Int]], isOpenCard: [[Bool]]) {
 
 
 
-var count = 0
-var collectedCount = 0
 
-var flatMapDeck = cardBoard.flatMap { $0 }
-var totalDeck = NSCountedSet(array: flatMapDeck)
 
 
 /// 카드 맞추기 게임 실행
 func playCardMatching() {
+    var count = 0
+    var collectedCount = 0
+    
+    var flatMapDeck = cardBoard.flatMap { $0 }
+    var totalDeck = NSCountedSet(array: flatMapDeck)
+    
+    var playerScore1 = 0
+    var playerScore2 = 0
+    var playerName1 = ""
+    var playerName2 = ""
+    
     var selectedCard = [(Int, Int)]()
     
+    // player 이름을 2개 받을 떄까지 반복
+    while playerName1.isEmpty && playerName2.isEmpty {
+        print("첫번째 플레이어 이름을 입력하세요")
+        guard let player1 = readLine(), !player1.isEmpty else {
+            print("한 글자이상 이름을 입력하세요")
+            continue
+        }
+        
+        print("두번째 플레이어 이름을 입력하세요")
+        guard let player2 = readLine(), !player2.isEmpty else {
+            print("한 글자이상 이름을 입력하세요")
+            continue
+        }
+        
+        playerName1 = player1
+        playerName2 = player2
+    }
+    
+    // 1P부터 게임 시작
+    var currentPlayer = playerName1
+    var baseScore = 10
+    
+    
     while (1...8).map({totalDeck.count(for: $0)}).filter({ $0 >= 2 }) != [] && collectedCount < 18 {
+        print("[\(playerName1)] \(playerScore1)점 vs [\(playerName2)] \(playerScore2)점")
+        print("---\(currentPlayer)님의 차례입니다.---")
         openCardBoard(cardBoard: cardBoard, isOpenCard: isOpenCard) // 1단계
         print("<시도 \(count + 1), 남은 카드: \(18 - collectedCount)> 좌표를 두번 입력하세요")
         
-       
+        
         guard let input = readLine()?.replacingOccurrences(of: "(", with: "")
             .replacingOccurrences(of: ")", with: ""),
               let row = Int(input.split(separator: ", ")[0]),
               let column = Int(input.split(separator: ", ")[1]),
               input.split(separator: ", ").count == 2,
               row >= 1 && row <= 3, column >= 1 && column <= 6 else {
-            print("[잘못된 입력] \n((1~3), (1~6))범위 이내로\n아래와 같이 숫자만 입력하세요\n예시)(1, 2)")
+            print("[잘못된 입력] \n((1~3), (1~6))범위 이내로\n아래와 같이 숫자만 입력하세요\n예시)(1, 2)\n")
             continue
         }
-        
         
         let selectedRow = row - 1
         let selectedColumn = column - 1
         
+//        guard let cardBoard[selectedRow][selectedColumn] > 0
+//        
+//        if cardBoard[selectedRow][selectedColumn] > 0 {
+//            isOpenCard[selectedRow][selectedColumn] = true
+//        }
         isOpenCard[selectedRow][selectedColumn] = true
-        
-      
         selectedCard.append((selectedRow, selectedColumn))
-      
+        
         
         if selectedCard.count == 2 {
             if selectedCard[0] != selectedCard[1] {
                 openCardBoard(cardBoard: cardBoard, isOpenCard: isOpenCard)
-                print("-----구분선-----")
+                print("-----구분선-----\n")
                 let first = selectedCard[0]
                 let second = selectedCard[1]
                 
                 if cardBoard[first.0][first.1] == cardBoard[second.0][second.1] {
-                    
-                    
                     count += 1
                     collectedCount += 2
                     isOpenCard[first.0][first.1] = true
@@ -119,25 +151,48 @@ func playCardMatching() {
                     flatMapDeck = cardBoard.flatMap { $0 }
                     totalDeck = NSCountedSet(array: flatMapDeck)
                     
+                    
+                    
+                    currentPlayer == playerName1 ? (playerScore1 += baseScore) : (playerScore2 += baseScore)
+                    baseScore *= 2
+                    
+                    
                 } else {
                     count += 1
+                    
                     isOpenCard[first.0][first.1] = false
                     isOpenCard[second.0][second.1] = false
+ 
+                    
+                    // 카드가 제거된 좌표와 오픈할려는 카드와 같이 고르면 제거된 카드 좌표에서 다시 X표시가 나타나는 문제를 해결
+                    if cardBoard[first.0][first.1] == 0 && cardBoard[second.0][second.1] > 0 {
+                        isOpenCard[first.0][first.1] = true
+                        print("첫 번째 카드는 이미 제거된 카드입니다. 턴이 넘어갑니다.")
+                    } else if cardBoard[first.0][first.1] > 0 && cardBoard[second.0][second.1] == 0 {
+                        isOpenCard[second.0][second.1] = true
+                        print("두 번째 카드는 이미 제거된 카드입니다. 턴이 넘어갑니다.")
+                    }
+                    
+                    
+                    currentPlayer == playerName1 ? (currentPlayer = playerName2) : (currentPlayer = playerName1)
+                    baseScore = 10
                 }
                 
             } else {
                 isOpenCard[selectedRow][selectedColumn] = false
-                print("[잘못된 입력] \n같은 좌표를 연속으로 입력하지 마세요")
+                print("[잘못된 입력] \n같은 좌표를 연속으로 입력하지 마세요\n")
             }
             selectedCard = [] //초기화
         }
     }
-    
-    print("축하합니다. 카드를 모두 맞췄습니다.")
+    print("\n축하합니다. 카드를 모두 맞췄습니다.")
+    print("---[최종 점수]---")
+    print("[\(playerName1)] \(playerScore1)점 vs [\(playerName2)] \(playerScore2)점")
+    if playerScore1 > playerScore2 {
+        print("\(playerName1)님이 우승하였습니다!")
+    } else {
+        print("\(playerName2)님이 우승하였습니다!")
+    }
 }
-
-
-
-
 
 playCardMatching()
